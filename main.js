e90d74e2b7072146969f506720104ced44b71954
Shlok Mishra <99207534+Jougan-0@users.noreply.github.com>
2023-01-25 18:47:18 +0530

    feat:responsive navbar

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("iva");
+const menuBtn=document.querySelector('.toogle-btn');
+const classes=document.querySelector('.navbar-links');
+menuBtn.addEventListener('click',function Onclick(){
+    classes.classList.toggle('active');
+})
